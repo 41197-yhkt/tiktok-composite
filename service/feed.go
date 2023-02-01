f6260c7111c8ae0bd3e7c343ed58b949c9bc9ca0
perhapzz <411600377@qq.com>
2023-02-01 11:31:35 +0800

    feat: perfect feed service

--- service/feed.go
+++ service/feed.go
@@ -19,41 +19,43 @@ func (s *FeedService) Feed(req *composite.BasicFeedRequest) ([]*composite.Vedio,
 	userDatabase := q.User.WithContext(s.ctx)
 	userFavoriteDatabase := q.UserFavorite.WithContext(s.ctx)
 
+	// 1. 查看 req.Last_time 是否存在，若存在使用 req.Last_time，否则使用 time.Now()
 	lastest_time := time.Now()
 	if req.LastestTime != nil {
 		lastest_time = time.Unix(*req.LastestTime, 0)
 	}
 
+	// 2. 查找 vedio 表中更新时间小于 last_time 的最大 limit 个视频
 	vedios, err := vedioDatabase.FindByUpdatedtime(lastest_time, 30)
 	if err != nil {
 		return nil, -1, err
 	}
-	// fmt.Println(vedios)
+
+	// 3. 根据每个 vedio
 	res := []*composite.Vedio{}
 	for _, vedio := range vedios {
-		// 根据 AuthorId 查作者信息
+		// 3.1. user 表中根据 AuthorId 查作者信息
 		author, err := userDatabase.FindByID(vedio.AuthorId)
 		if err != nil {
 			panic(err)
 		}
 
-		// TODO: 根据 AuthorId 查作者的粉丝和关注数
-		// followCount, followercount := 1, 1
-
-		// 根据 user_id 和 vedio_id 查是否点赞了
+		// 3.2. user_favorite 表中根据 user_id 和 vedio_id 查是否点赞了
+		// TODO:  可以用 EXISTS 优化？
 		var isFavorite bool
 		err = userFavoriteDatabase.FindByUseridAndVedioid(vedio.AuthorId, int64(vedio.ID))
 		if err == nil {
 			isFavorite = true
 		}
 
+		// 4. 封装
 		res = append(res, &composite.Vedio{
 			Id: int64(vedio.ID),
 			Author: &composite.User{
-				Id:   author.Id,
-				Name: author.Name,
-				// FollowCount: followCount,
-				// FollowerCount: &followercount,
+				Id:            int64(author.ID),
+				Name:          author.Name,
+				FollowCount:   &author.FollowCount,
+				FollowerCount: &author.FollowerCount,
 			},
 			PlayUrl:    vedio.PlayUrl,
 			CoverUrl:   vedio.CoverUrl,
