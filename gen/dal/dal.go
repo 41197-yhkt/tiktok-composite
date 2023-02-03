0721e713c113bca41029c8a9ddfed376a2df71fc
perhapzz <48175595+perhapzz@users.noreply.github.com>
2023-02-03 19:46:23 +0800

    refactor: modify database interface (#18)

--- gen/dal/dal.go
+++ gen/dal/dal.go
@@ -15,12 +15,12 @@ var once sync.Once
 func Init() {
 	once.Do(func() {
 		DB = ConnctDB().Debug()
-		_ = DB.AutoMigrate(&model.Vedio{}, &model.UserFavorite{})
+		_ = DB.AutoMigrate(&model.UserFavorite{}, &model.Comment{})
 	})
 }
 
 func ConnctDB() (conn *gorm.DB) {
-	conn, err := gorm.Open(mysql.Open("gorm:gorm@tcp(localhost:9910)/gorm?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	conn, err := gorm.Open(mysql.Open("root:123456@tcp(localhost:6666)/tiktok_db?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
